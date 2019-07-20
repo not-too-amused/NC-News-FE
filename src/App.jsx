b8cd4e659185a5e7f6e216cbc58cc792be7c43be
ian <kennaleeds@gmail.com>
2019-07-20 14:19:35 +0100

    removes defunct code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/Header.jsx";
-import HomeNav from "./components/Nav.jsx";
+import Nav from "./components/Nav.jsx";
 import Topics from "./components/Topics"
 import Articles from "./components/Articles"
 import ArticleCard from "./components/ArticleCard"
@@ -20,7 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header className="Header" />
-        <HomeNav className="Nav" path="/" />
+        <Nav className="Nav" path="/" />
         <Router className="Main">
           <Topics path="/" />
           <Articles path="/articles/all/" articles={this.state.articles} />
